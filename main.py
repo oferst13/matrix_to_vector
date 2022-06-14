d8f59d17010be00b0a5561ee9b0fd90d3f4e0bdc
oferst13 <66622305+oferst13@users.noreply.github.com>
2022-06-14 13:32:35 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+mat = np.random.randint(100, size=(100, 100, 1000))
+row = 15
+column = 22
+
+# save the vector of all integers from the given row and column
+# find the sum, min, max and average
